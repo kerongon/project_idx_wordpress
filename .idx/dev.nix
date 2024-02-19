960f92bce28f27ebdc90744e7d96e0250d428fa9
kerongonz <kerongonz@gmail.com>
2024-02-19 19:46:38 +0000

    start docker view

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,6 +7,7 @@
     pkgs.php
     pkgs.unzip
     pkgs.docker-compose
+    pkgs.mariadb
   ];
   # Sets environment variables in the workspace
   env = {
@@ -29,9 +30,19 @@
   idx.previews = {
     enable = true;
     previews = [
+      # {
+      #   command = ["/usr/bin/php" "-S" "0.0.0.0:$PORT"];
+      #   cwd = "www";
+      #   manager = "web";
+      #   id = "web";
+      # }
       {
-        command = ["/usr/bin/php" "-S" "0.0.0.0:$PORT"];
-        cwd = "www";
+        # command = ["/usr/bin/docker" "run" "-p" "$PORT:80" "-v" "/home/user/project_idx_wordpress/www:/var/www/html" "--network" "idx_default" "idx-wordpress-php-apache:latest"];
+        "command" =  [
+        "/bin/bash" "-c"
+        "if ! /usr/bin/docker ps | grep -q mariadb; then /usr/bin/docker start idx-db-1; fi; /usr/bin/docker run -p 9002:80 -v /home/user/project_idx_wordpress/www:/var/www/html --network idx_default idx-wordpress-php-apache:latest"
+        ];
+        cwd = ".";
         manager = "web";
         id = "web";
       }
